e88ed66310097e1b16d0bcd89bcf267af58ccc08
Yoshimasa Tanabe <yoshimasa_tanabe@funds.jp>
2021-07-31 17:33:43 +0900

    Add chapter-up-and-running

--- build.sbt
+++ build.sbt
@@ -4,4 +4,6 @@ version := "1.0"
 
 organization := "com.manning"
 
+lazy val up = project.in(file("chapter-up-and-running"))
+
 Test / parallelExecution := false
---
+++ chapter-up-and-running/src/main/scala/com/goticks/Main.scala
@@ -0,0 +1,50 @@
+package com.goticks
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object Main extends App with RequestTimeout {
+  val config = ConfigFactory.load()
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContext = system.dispatcher
+
+  val api = new RestApi(system, requestTimeout(config)).routes
+
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val bindingFuture: Future[ServerBinding] =
+    Http().bindAndHandle(api, host, port)
+
+  val log = Logging(system.eventStream, "go-ticks")
+  bindingFuture
+    .map { serverBinding =>
+      log.info(s"RestApi bound to ${serverBinding.localAddress}")
+    }
+    .onComplete {
+      case Success(_) =>
+        log.info(s"Success to bind to $host:$port")
+      case Failure(ex) =>
+        log.error(ex, s"Failed to bind to $host:$port")
+        system.terminate()
+    }
+}
+
+trait RequestTimeout {
+  import scala.concurrent.duration._
+  def requestTimeout(config: Config): Timeout = {
+    val t = config.getString("akka.http.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
+}
